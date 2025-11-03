834b296986dc8c606adc126eecc16ff7f76dddcb
ahmedasaber <ahmeda.saber22@gmail.com>
2025-11-03 17:40:52 +0200

    add paypal payment entity

---
+++ lib/features/checkout/domain/enities/payment entity/amount_entity.dart
@@ -0,0 +1,27 @@
+import 'package:fruits_ecommerce/core/helper_function/get_currency.dart';
+import 'package:fruits_ecommerce/features/checkout/domain/enities/order_entity.dart';
+import 'package:fruits_ecommerce/features/checkout/domain/enities/payment%20entity/details_entity.dart';
+
+class AmountEntity {
+  final String total;
+  final String currency;
+  final DetailsEntity details;
+
+  AmountEntity({
+    required this.total,
+    required this.currency,
+    required this.details,
+  });
+
+  factory AmountEntity.fromEntity(OrderEntity entity) {
+    return AmountEntity(
+      total: entity.calcTotalPriceAfterShippingAndDiscount().toString(),
+      currency: getCurrency(),
+      details: DetailsEntity.fromEntity(entity),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {'total': total, 'currency': currency, 'details': details.toJson()};
+  }
+}
